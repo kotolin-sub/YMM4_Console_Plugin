1c97fc97b3c1d57a5448e05d4618ea97e92b1183
kotolin-sub <tonuma777@gmail.com>
2024-06-21 18:47:12 +0900

    Update sorce.cs

--- sorce.cs
+++ sorce.cs
@@ -1,9 +1,9 @@
-using Microsoft.Win32.SafeHandles;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.Json;
+using Microsoft.Win32.SafeHandles;
 using YukkuriMovieMaker.Plugin;
 
 namespace YmmConsole
@@ -13,6 +13,7 @@ namespace YmmConsole
         static YmmDebugWindowPlugin()
         {
             AllocConsole();
+
             var settingsPath = @".\user\setting\4.28.2.1\YukkuriMovieMaker.Settings.YMMSettings.json";
             if (JsonDocument.Parse(File.ReadAllText(settingsPath)).RootElement.TryGetProperty("IsKawaiiMode", out JsonElement nameElement))
             {
@@ -21,12 +22,8 @@ namespace YmmConsole
                 Console.Clear();
             }
 
-            // 標準出力のリダイレクト
-            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, ownsHandle: false);
-            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
-            StreamWriter standardOutput = new StreamWriter(fileStream) { AutoFlush = true };
-            Console.SetOut(standardOutput);
+            // 標準出力と標準エラーのリダイレクト
+            RedirectStandardOutputAndError();
         }
 
         [DllImport("kernel32.dll")]
@@ -36,6 +33,24 @@ namespace YmmConsole
         private static extern IntPtr GetStdHandle(int nStdHandle);
 
         private const int STD_OUTPUT_HANDLE = -11;
+        private const int STD_ERROR_HANDLE = -12;
+
+        private static void RedirectStandardOutputAndError()
+        {
+            // 標準出力のリダイレクト
+            IntPtr stdOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeStdOutHandle = new SafeFileHandle(stdOutHandle, ownsHandle: false);
+            FileStream stdOutStream = new FileStream(safeStdOutHandle, FileAccess.Write);
+            StreamWriter stdOutWriter = new StreamWriter(stdOutStream) { AutoFlush = true };
+            Console.SetOut(stdOutWriter);
+
+            // 標準エラー出力のリダイレクト
+            IntPtr stdErrHandle = GetStdHandle(STD_ERROR_HANDLE);
+            SafeFileHandle safeStdErrHandle = new SafeFileHandle(stdErrHandle, ownsHandle: false);
+            FileStream stdErrStream = new FileStream(safeStdErrHandle, FileAccess.Write);
+            StreamWriter stdErrWriter = new StreamWriter(stdErrStream) { AutoFlush = true };
+            Console.SetError(stdErrWriter);
+        }
 
         public static void ConBridge(object text)
         {
