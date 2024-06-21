7f5b4d228372289497a1cb6c61949fa5509a24c1
kotolin-sub <tonuma777@gmail.com>
2024-06-21 17:28:07 +0900

    Update sorce.cs

--- sorce.cs
+++ sorce.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using YukkuriMovieMaker.Plugin;
 
@@ -8,25 +8,50 @@ namespace YmmConsole
     {
         static YmmDebugWindowPlugin()
         {
-            [DllImport("kernel32.dll")]
-            static extern bool AllocConsole();
             AllocConsole();
-            if (JsonDocument.Parse(File.ReadAllText(@".\user\setting\4.28.2.1\YukkuriMovieMaker.Settings.YMMSettings.json")).RootElement.TryGetProperty("IsKawaiiMode", out JsonElement nameElement)) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; Console.Clear(); }
+            var settingsPath = @".\user\setting\4.28.2.1\YukkuriMovieMaker.Settings.YMMSettings.json";
+            if (JsonDocument.Parse(File.ReadAllText(settingsPath)).RootElement.TryGetProperty("IsKawaiiMode", out JsonElement nameElement))
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Clear();
+            }
             Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
         }
-        public static void ConBridge(object text)
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        public static void DebugPrint(object text)
         {
             Console.WriteLine(text);
         }
+        public static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
         public string Name => "DebugWindow";
     }
 
     public class YmmCon
     {
-        public static void DebugPrint(object text)
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+    }
+
+    public static class YMMConsole
+    {
+        public static void WriteLine(object text)
         {
-            YmmDebugWindowPlugin.ConBridge(text);
+            YmmDebugWindowPlugin.DebugPrint(text);
+        }
+        public static string ReadLine() { 
+            return YmmDebugWindowPlugin.ReadLine();
         }
     }
 }
-
