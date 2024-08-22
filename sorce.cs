27753137a07d02677ecf60446f384c8e7a2cb7c1
kotolin-sub <tonuma777@gmail.com>
2024-08-22 19:29:02 +0900

    Update sorce.cs

--- sorce.cs
+++ sorce.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Diagnostics;
-using System.IO;
+using Microsoft.Win32.SafeHandles;
 using System.Runtime.InteropServices;
 using System.Text.Json;
-using Microsoft.Win32.SafeHandles;
 using YukkuriMovieMaker.Plugin;
 
 namespace YmmConsole
@@ -14,16 +11,7 @@ namespace YmmConsole
         {
             AllocConsole();
 
-            var settingsPath = @".\user\setting\4.28.2.1\YukkuriMovieMaker.Settings.YMMSettings.json";
-            if (JsonDocument.Parse(File.ReadAllText(settingsPath)).RootElement.TryGetProperty("IsKawaiiMode", out JsonElement nameElement))
-            {
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.Clear();
-            }
-
-            // 標準出力と標準エラーのリダイレクト
-            RedirectStandardOutputAndError();
+            RedirectOutput();
         }
 
         [DllImport("kernel32.dll")]
@@ -35,16 +23,14 @@ namespace YmmConsole
         private const int STD_OUTPUT_HANDLE = -11;
         private const int STD_ERROR_HANDLE = -12;
 
-        private static void RedirectStandardOutputAndError()
+        private static void RedirectOutput()
         {
-            // 標準出力のリダイレクト
             IntPtr stdOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
             SafeFileHandle safeStdOutHandle = new SafeFileHandle(stdOutHandle, ownsHandle: false);
             FileStream stdOutStream = new FileStream(safeStdOutHandle, FileAccess.Write);
             StreamWriter stdOutWriter = new StreamWriter(stdOutStream) { AutoFlush = true };
             Console.SetOut(stdOutWriter);
 
-            // 標準エラー出力のリダイレクト
             IntPtr stdErrHandle = GetStdHandle(STD_ERROR_HANDLE);
             SafeFileHandle safeStdErrHandle = new SafeFileHandle(stdErrHandle, ownsHandle: false);
             FileStream stdErrStream = new FileStream(safeStdErrHandle, FileAccess.Write);
@@ -73,4 +59,5 @@ namespace YmmConsole
         [DllImport("kernel32.dll")]
         public static extern bool ReadProcessMemory(int hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
     }
+    
 }
